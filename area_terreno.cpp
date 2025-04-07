23bdb6c8160b002f030f528db2a6d6a45bc96cbf
Larissa Alves de Souza <162065841+Larissa-alSouza@users.noreply.github.com>
2025-04-07 20:31:02 -0300

    Create area_terreno.cpp

---
+++ area_terreno.cpp
@@ -0,0 +1,18 @@
+#include<stdio.h>
+
+main()
+{
+	float l, c, ar;
+	
+	printf("\t\t Calcula area de terrenos \n\n");
+	
+	printf("Digite a largura do terreno: ");
+	scanf("%f", &l);
+	
+	printf("Digite o comprimento do terreno: ");
+	scanf("%f", &c);
+	
+	ar = l * c;
+	
+	printf("A area do terreno do terreno de largura %.2f e comprimento %.2f = %.2f", l, c, ar);
+}
