02e0336b63fc1ecd50035ac4501af963dd957939
Larissa Alves de Souza <162065841+Larissa-alSouza@users.noreply.github.com>
2025-04-10 15:33:59 -0300

    Create exercicio_2.cpp

---
+++ Linguagem C/Lista de Exercícios 1/exercicio_2.cpp
@@ -0,0 +1,18 @@
+/* 1- Leia um número e mostre como seu resultado seu antecessor e seu sucessor */
+
+#include <stdio.h>
+
+int main() {
+    
+    float num, ant, suc;
+    
+    printf("Insira uma número: ");
+    scanf("%f", &num);
+    
+    ant = num - 1;
+    suc = num + 1;
+    
+    printf("Antecessor: %.2f e Sucessor: %.2f", ant, suc);
+
+    return 0;
+}
