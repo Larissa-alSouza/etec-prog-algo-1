9301af8ae469c080e06bf264404405e70786b8d5
Larissa Alves de Souza <162065841+Larissa-alSouza@users.noreply.github.com>
2025-04-21 19:28:45 -0300

    Create exercicio_3.cpp

---
+++ Linguagem C/Lista de Exercícios 4/exercicio_3.cpp
@@ -0,0 +1,15 @@
+/*  3- Gerar os valores em ordem decrescente de 500 a 0 */
+
+#include <stdio.h>
+
+int main() {
+    
+    int num;
+
+    for(num = 500; num >= 0; num--)
+    {
+        printf("%d \n", num);
+    }
+    
+    return 0;
+}
