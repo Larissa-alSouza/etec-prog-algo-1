65e80ff3cda4b53a8303fec41bfc86612f871ee4
Larissa Alves de Souza <162065841+Larissa-alSouza@users.noreply.github.com>
2025-05-08 20:41:38 -0300

    Create exercicio_2.cpp

---
+++ Linguagem C++/Lista de Exercícios 2/exercicio_2.cpp
@@ -0,0 +1,27 @@
+/* 2- Faça um algoritmo que leia a idade de uma pessoa expressa em anos, meses e dias e converta-a para apenas em dias */
+
+#include <locale.h>
+#include <iostream>
+using namespace std;
+
+int main() {
+    
+    int a, m, d, as, ms, result;
+    
+    cout << "Insira os anos: ";
+    cin >> a;
+    
+    cout << "Insira os meses: ";
+    cin >> m;
+    
+    cout << "Insira os dias: ";
+    cin >> d;
+    
+    as = a * 365;
+    ms = m * 30;
+    result = as + ms + d;
+    
+    cout << "Dias: " << result << "\n";
+
+    return 0;
+}
