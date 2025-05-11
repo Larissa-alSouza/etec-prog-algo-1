b64dbca2c300d21e70c2ccc0537c3627cecdb497
Larissa Alves de Souza <162065841+Larissa-alSouza@users.noreply.github.com>
2025-05-11 01:50:54 -0300

    Create exercicio_2.cpp

---
+++ Linguagem C++/Lista de Exercícios 5/exercicio_2.cpp
@@ -0,0 +1,38 @@
+/* 2- Solicitar a idade de várias pessoas e imprimir: Quantidade de pessoas com menos de 21 anos. Quantidade de pessoas com mais de 50 anos. O programa termina quando idade digitada for -99 */
+
+#include <locale.h>
+#include <iostream>
+using namespace std;
+
+int main() 
+{
+    setlocale(LC_ALL, "Portuguese");
+    
+    int idade, soma1 = 0, soma2 = 0, contador = 0;
+
+    while (true) {
+        
+        cout << "Digite as idades (digite -99 para encerrar):\n";
+            
+        cin >> idade;
+
+        if (idade == -99)
+        {
+            break;
+        }
+        
+        if (idade < 21) 
+        {
+            soma1 += 1;
+        } 
+        else 
+        {
+            soma2 += 1;
+        }
+    }
+    
+        cout << "Menos 21: " << soma1 << "\n";
+        cout << "Mais 50: " << soma2 << "\n";
+
+    return 0;
+}
