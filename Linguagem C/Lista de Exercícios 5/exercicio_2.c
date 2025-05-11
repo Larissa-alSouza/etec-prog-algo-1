b2659494c665c34bed7bf7fcca0ce0f639464042
Larissa Alves de Souza <162065841+Larissa-alSouza@users.noreply.github.com>
2025-05-11 01:58:40 -0300

    Create exercicio_2.c

---
+++ Linguagem C/Lista de Exercícios 5/exercicio_2.c
@@ -0,0 +1,37 @@
+/* 2- Solicitar a idade de várias pessoas e imprimir: Quantidade de pessoas com menos de 21 anos. Quantidade de pessoas com mais de 50 anos. O programa termina quando idade digitada for -99 */
+
+#include <stdio.h>
+#include <locale.h>
+#include <stdbool.h>
+
+int main() 
+{
+    setlocale(LC_ALL, "Portuguese");
+    
+    int idade, soma1 = 0, soma2 = 0;
+
+    while (true) {
+        
+        printf("Digite as idades (digite -99 para encerrar):\n");
+        scanf("%d", &idade);
+
+        if (idade == -99)
+        {
+            break;
+        }
+        
+        if (idade < 21) 
+        {
+            soma1 += 1;
+        } 
+        else if(idade >= 50)
+        {
+            soma2 += 1;
+        }
+    }
+    
+        printf("Menos 21: %d", soma1);
+        printf("\nMais 50: %d", soma2);
+
+    return 0;
+}
