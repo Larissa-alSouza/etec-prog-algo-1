cdbecef887e8b984531573eafda090855e446949
Larissa Alves de Souza <162065841+Larissa-alSouza@users.noreply.github.com>
2025-05-19 19:57:12 -0300

    Create exercicio_4.cpp

---
+++ Linguagem C++/Lista de Exercícios 5/exercicio_4.cpp
@@ -0,0 +1,26 @@
+#include <locale.h>
+#include <iostream>
+using namespace std;
+
+main()
+{
+	setlocale (LC_ALL, "Portuguese");
+	
+	int num, soma = 0, media = 0, count = 0;
+	
+	do
+	{
+		cout << "Insira um número: ";
+		cin >> num;
+		
+		if(num > 0)
+		{
+			soma += num;
+			count++;
+		}
+	}while(num > 0);
+	
+	media = soma / count;
+	
+	cout << "A média foi de: " << media << "\n";
+}
