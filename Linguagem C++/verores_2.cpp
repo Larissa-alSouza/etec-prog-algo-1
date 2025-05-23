16aa1538bac16cf4e54d3d76c8e6b8f8a53a040f
Larissa Alves de Souza <162065841+Larissa-alSouza@users.noreply.github.com>
2025-05-22 22:24:15 -0300

    Create verores_2.cpp

---
+++ Linguagem C++/verores_2.cpp
@@ -0,0 +1,66 @@
+#include<iostream>
+#include<conio.h>
+#include<locale.h>
+using namespace std;
+
+main()
+{
+	setlocale(LC_ALL, "Portuguese");
+	
+	int x[20], i, p, y, flag, q;
+	float soma = 0, media = 0;
+	
+	do
+	{
+		cout << "Quantos valores deseja inserir na lista? (máx. 20): ";
+		cin >> q;
+	}while(q > 20);
+	
+	cout << "Digite uma lista com " << q << " valores para calcular a média \n";
+	
+	for(i = 0; i < q; i++)
+	{
+		cin >> x[i];
+	}
+	
+	cout << "\n\n Lista de valores\n\n";
+	system("cls");
+	
+	for(i = 0; i < q; i++)
+	{
+		soma+= x[i];
+		cout << x[i] << " ";
+	}
+	
+	media = soma/q;
+	cout<< "Média dos valores: " << media << "\n";
+	
+	cout << "Deseja verificar o valor de qual posição da lista: ";
+	cin >> p;
+	
+	cout << "O valor escolhido da posição " << p << " é o valor " << x[p - 1] << "\n";
+	
+	flag = -1;
+	cout << "Qual número deseja encontrar na lista: ";
+	cin >> y;
+	
+	for(i = 0; i < 20; i++)
+	{
+		if(y == x[i])
+		{
+			flag = i;
+			i = 19;
+		}
+	}
+	
+	if(flag != -1)
+	{
+		cout << "O valor " << y << " se encontra na posição " << flag + 1 << "\n";
+	}
+	else
+	{
+		cout << "O valor " << y << " não se encontra em nenhuma posição da lista \n\n";
+	}
+	
+	system("pause");
+}
