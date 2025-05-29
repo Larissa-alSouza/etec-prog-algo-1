b2d9cc8a03e8cc59a82b5309f1e9a1475b1177f4
Larissa Alves de Souza <162065841+Larissa-alSouza@users.noreply.github.com>
2025-05-28 21:15:41 -0300

    Create exercicio_7.cpp

---
+++ Linguagem C++/Lista de Exercícios 6/exercicio_7.cpp
@@ -0,0 +1,31 @@
+#include<iostream>
+#include<locale.h>
+using namespace std;
+
+main()
+{
+	setlocale(LC_ALL, "Portuguese");
+	
+	int v1[5], v2[5], i, x;
+	
+	for(i = 0; i < 5; i++)
+	{
+		cout << "Digite um número: ";
+		cin >> v1[i];
+	}
+	
+	x = v1[0];
+	
+	for(i = 0; i < 5; i++)
+	{
+		if(x < v1[i])
+		{
+			x = v1[i];
+		}
+	}
+	
+	for(i = 0; i < 5; i++)
+	{
+		v2[i] = v1[i] * x;
+	}
+}
