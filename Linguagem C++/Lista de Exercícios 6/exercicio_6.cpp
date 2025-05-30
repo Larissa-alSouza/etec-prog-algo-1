b7e69bdd6c865fd5ec1c2f993eb9a06c2c3ec868
Larissa Alves de Souza <162065841+Larissa-alSouza@users.noreply.github.com>
2025-05-29 22:37:34 -0300

    Update exercicio_6.cpp

--- Linguagem C++/Lista de Exercícios 6/exercicio_6.cpp
+++ Linguagem C++/Lista de Exercícios 6/exercicio_6.cpp
@@ -6,29 +6,40 @@ main()
 {
 	setlocale(LC_ALL, "Portuguese");
 	
-	int ano[10], idade[10], i, ano_atual;
+	char nome[10][30];
+	int ano[10], idade[10], i, ano_atual, n;
 	float media = 0, soma = 0;
 	
 	cout << "Em que ano estamos? ";
 	cin >> ano_atual;
 	
-	for(i = 0; i < 10; i++)
+	do
 	{
-		cout << "Digite o ano do seu nascimento: ";
+		cout << "Quantas pessoas serão inseridas? ";
+		cin >> n;
+	} while(n> 10);
+	
+	for(i = 0; i < n; i++)
+	{
+		fflush(stdin);
+		cout << "Digite o nome da " << i+1 << " pessoa: ";
+		gets(nome[i]);
+		
+		cout << "Digite o ano do nascimento de  " << nome[i] << ": ";
 		cin >> ano[i];
 	}
 	
-	for(i = 0; i < 10; i++)
+	for(i = 0; i < n; i++)
 	{
 		idade[i] = ano_atual - ano[i];
 		
 	}
 	
-	for(i = 0; i < 10; i++)
+	for(i = 0; i < n; i++)
 	{
-		cout << "Idade da pessoa " << (i + 1) << ": " << idade[i] << "\n";
+		cout << "Idade de " << nome[i] << ": " << idade[i] << "anos \n";
 		soma += idade[i];
-		media = soma / 10;
+		media = soma / n;
 	}
 	
 	cout << soma << "\n";
